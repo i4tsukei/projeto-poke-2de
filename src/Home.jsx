75b8be12c2edf1f9baed8e5cf432f8c7e12da20f
i4tsukei <ligia.direito2014@gmail.com>
2025-05-22 12:59:58 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,20 @@
 import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
 import './Home.css'
-
 const Home = () => {
-
+    
     return (
         <div>
           
             <div>
                 <img src={poke} />
+                <img src={eevee} />
                 <img src={poke} />
+                <img src={eevee} />
                 <img src={poke} />
+                <img src={eevee} />
                 <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
+                <img src={eevee} />
                 <img src={poke} />
             </div>
             
