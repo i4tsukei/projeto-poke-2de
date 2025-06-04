a75c10819895d1288fad7ccbc2c293d769a8c1b7
i4tsukei <ligia.direito2014@gmail.com>
2025-06-04 11:15:08 +0000

    grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,20 @@ import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
 import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
+ 
 const Home = () => {
-    
+ 
     return (
+       
         <div>
-            <PokeFlex/>
+         
+          <PokeFlex/>
+           <PokeGrid/>
+ 
         </div>
+     
     )
 }
-
+ 
 export default Home
